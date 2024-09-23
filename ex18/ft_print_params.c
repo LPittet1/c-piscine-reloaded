0f907dc82478f9c9c0662b9b357c1efd4d16beb1
LPittet1 <pittet82@gmail.com>
2024-09-23 14:21:00 +0200

    ex 16-18

---
+++ ex18/ft_print_params.c
@@ -0,0 +1,26 @@
+void ft_putchar(char c);
+
+void    ft_putstr(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+    {
+        ft_putchar(str[i]);
+        i++;
+    }
+}
+
+int main (int ac, char **av)
+{
+    int i;
+
+    i = 1;
+    while (i < ac)
+    {
+        ft_putstr(av[i]);
+        ft_putchar('\n');
+        i++;
+    }
+}
