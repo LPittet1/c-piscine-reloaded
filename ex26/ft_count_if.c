5c4e0054fdf9327869f4a5f3566515f9f70dc765
LPittet1 <pittet82@gmail.com>
2024-09-29 19:46:17 +0200

    last norm

--- ex26/ft_count_if.c
+++ ex26/ft_count_if.c
@@ -1,15 +1,27 @@
-int ft_count_if(char **tab, int (*f)(char*))
-{
-    int i;
-    int count;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_count_if.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpittet <lpittet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/29 19:45:10 by lpittet           #+#    #+#             */
+/*   Updated: 2024/09/29 19:45:50 by lpittet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    i       = 0;
-    count   = 0;
-    while(tab[i])
-    {
-        if ((*f)(tab[i]))
-            count++;
-        i++;
-    }
-    return (count);
+int	ft_count_if(char **tab, int (*f)(char*))
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (tab[i])
+	{
+		if ((*f)(tab[i]))
+			count++;
+		i++;
+	}
+	return (count);
 }
